dd3064517d13da62da348203c5be9bd270b90dad
Iamneo <techadmin@iamneo.ai>
2023-11-15 05:18:31 +0000

    Backup Commit

--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -4,10 +4,19 @@ using dotnetapp.Models;
 namespace dotnetapp.Models
 {
     public class ApplicationDbContext : DbContext
-    {
+    {       
+         public ApplicationDbContext(){
+                
+            }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseSqlServer("User ID = sa; password = examlyMssql@123; server = localhost; Database = webDB; trusted_connetion = false")
+            }
         }
 
         // Add DbSet properties for other entities as needed
