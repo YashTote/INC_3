7268c1dac79697136c09d8360ddedcf15fe03fc5
Iamneo <techadmin@iamneo.ai>
2023-11-20 06:07:31 +0000

    Backup Commit

--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -17,7 +17,7 @@ namespace dotnetapp.Models
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if(!optionsBuilder.IsConfigured){
-                optionsBuilder.UseSqlServer("User ID = sa; password = examlyMssql@123; server = localhost; Database = inc3DB; trusted_connetion = false; Persist Security Info = false; Encrypt = false");
+                optionsBuilder.UseSqlServer("User ID = sa; password = examlyMssql@123; server = localhost; Database = inc3DB; trusted_connection = false; Persist Security Info = false; Encrypt = false");
             }
         }
 
