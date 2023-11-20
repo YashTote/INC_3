bc6d5b804dc7a0687a2e65e9de81968e26f00e5f
Iamneo <techadmin@iamneo.ai>
2023-11-20 10:37:13 +0000

    Backup Commit

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,13 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
-  login(){
-    
+  private url  = 'https://8080-aebcdfaefacffabcaaaceeafebecebbffdafdefabcc.premiumproject.examly.io/Movie';
+
+  constructor(private httpclient : HttpClient) { }  
+  login() : Observable<{role : string}>{
+    return  ({role : 'ADMIN'});
   }
 
-  constructor() { }
 }
