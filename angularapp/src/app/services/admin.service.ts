d9c74ac7b238106275ddb8f3d092bdd089a2a619
Iamneo <techadmin@iamneo.ai>
2023-11-20 11:52:12 +0000

    Backup Commit

--- angularapp/src/app/services/admin.service.ts
+++ angularapp/src/app/services/admin.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  newPlayer : {id:number, name:string, age:number, category:string, biddingPrice: number};
+  
+  private url= "string";
+  constructor(private httpclient : HttpClient) { }
+  getTeams():Observable<any[]>{
+    return;
+  }
+  createTeam(newPlayer) : Observable<any[]>{
+    return
+  }
+  getPlayers():Observable<any[]>{
+    return
+  }
 
-  constructor() { }
 }
